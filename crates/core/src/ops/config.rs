8edc6bc2df45d8d18a7b4727f700604d42d4d26b
Doug Anderson444 <douganderson444@gmail.com>
2024-09-10 17:39:52 -0300

    re-export utils types

--- crates/core/src/ops/config.rs
+++ crates/core/src/ops/config.rs
@@ -1,6 +1,8 @@
 pub(crate) mod utils;
 use utils::*;
-pub use utils::{LockScript, UnlockScript, VladCid, VladConfig};
+pub use utils::{
+    CidGen, KeyParams, LockScript, UnlockScript, UseStr, VladCid, VladConfig, VladKey,
+};
 
 use serde::{Deserialize, Serialize};
 pub(crate) mod defaults;
