87a7ab9a67b2fdb8f75540686b18ae361bc030ec
Doug Anderson444 <douganderson444@gmail.com>
2024-10-03 20:52:08 -0300

    add Connect component + Header

--- demo/src/lib/components/Header.svelte
+++ demo/src/lib/components/Header.svelte
@@ -1,27 +1,36 @@
 <!-- Use lib/components/graphics/disconnected.svelte to show Modal.svelte of Connect.svelte -->
 <script>
+	import { onMount } from 'svelte';
 	import Connect from '$lib/components/Connect.svelte';
 	import Disconnect from '$lib/components/graphics/Disconnected.svelte';
 	import Modal from './Modal.svelte';
+	import { resolveDnsaddr } from '$lib/utils/index.js';
 
 	/** @type {boolean} */
 	let showModal = false;
+
+	let dialAddr = '/dnsaddr/peerpiper.io';
+
+	onMount(async () => {
+		let res = await resolveDnsaddr(dialAddr);
+		console.log(res);
+	});
 
 	function toggleModal() {
 		showModal = !showModal;
 	}
 </script>
 
-<div
+<button
 	class="absolute top-0 right-0 m-2 p-2 cursor-pointer border-neutral-100 rounded-lg bg-white"
 	on:click={toggleModal}
 	aria-label="Connection Settings"
 >
 	<Disconnect />
-</div>
+</button>
 
 {#if showModal}
 	<Modal title="Connection Settings" on:close={toggleModal}>
-		<Connect />
+		<Connect {dialAddr} />
 	</Modal>
 {/if}
